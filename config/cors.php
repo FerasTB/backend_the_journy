8051e8be1392d90ca33b4fc4487f38d8c29658d2
FerasTB <al2mohtrf@gmail.com>
2024-09-07 20:33:58 +0300

    cros 2

--- config/cors.php
+++ config/cors.php
@@ -17,13 +17,13 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET, POST, PUT, DELETE, PATCH, OPTIONS'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000', '*'],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['Content-Type, X-Auth-Token, Authorization, Origin'],
 
     'exposed_headers' => [],
 
